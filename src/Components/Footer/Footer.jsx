1aeb2b232ce0f01f5bb8d2e6146ef7f80873c338
PessoaKauan <kauankpp@hotmail.com>
2024-07-12 17:05:24 -0300

    Desafio Neki web

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+    return (
+      
+        <footer className="footer">
+          <a className="footer2"> Neki-Skills ©2024 Criado por Kauan</a>
+        </footer>
+    )
+}
+
+export default Footer;
